2b138f4fa7a5c2b0d428a910111644b55d24f7cf
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-11 18:36:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |name|
+      puts name
+    end
+  end
+  
+end
